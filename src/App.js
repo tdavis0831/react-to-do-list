4d7798f859b752a626877c7c332ed3a6daf9469d
Tasha Davis <tdavis0831@gmail.com>
2021-10-22 04:26:34 +0000

    text box takes input and then clears out with setInputText, updated with todos added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css'; 
 import Form  from "./components/form";
+import ToDoList from "./components/ToDoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
       <h1>Tasha's To Do List</h1>
       </header>
-      <Form />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+      <ToDoList />
     </div>
   );
 }
