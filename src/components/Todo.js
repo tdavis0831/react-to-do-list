af8b739d6af4ba1e365b7fde9c65c3ee48475978
Tasha Davis <tdavis0831@gmail.com>
2021-10-31 16:09:00 +0000

    check mark creates a linethrough item on list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,11 +5,23 @@ const Todo = ({text, todos, todo, setTodos}) =>{
     const deleteHandler = () => {
         setTodos(todos.filter((el) => el.id !== todo.id))
     };
+    const completeHandler = () => {
+        setTodos(todos.map(item =>{
+            if(item.id === todo.id){
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+                return item;
+            
+            
+        }));
+    }
     return (
         <div className="todo">
-            <li className="todo-item">{text}</li>
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
 
-        <button className="complete-btn" > 
+        <button onClick={completeHandler} className="complete-btn" > 
         <i className= "fas fa-check"> </i> 
         </button>
 
